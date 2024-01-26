eacb328f8c73bf4fadcf82af7e4e38c649b6ec70
Fusu <Arkadiusz.fus@codebind.pl>
2024-01-26 20:01:52 +0100

    Update Battle.svelte

--- src/components/Battle.svelte
+++ src/components/Battle.svelte
@@ -33,10 +33,14 @@ const unitsStore = {
 };
 
 
-unitsStore.addUnit({id: 'player1', width: 0.1, aspectRadio: 16/9, place: 1, team: 'player', hitPoints: 5, maxHitPoints: 5, speed: 100, attack: 1})
-unitsStore.addUnit({id: 'player2', width: 0.1, aspectRadio: 16/9, place: 2, team: 'player', hitPoints: 5, maxHitPoints: 5, speed: 100, attack: 1})
-unitsStore.addUnit({id: 'enemy1', width: 0.1, aspectRadio: 16/9, place: 1, team: 'enemy', hitPoints: 4, maxHitPoints: 4, speed: 90, attack: 1})
-unitsStore.addUnit({id: 'enemy2', width: 0.1, aspectRadio: 16/9, place: 2, team: 'enemy', hitPoints: 4, maxHitPoints: 4, speed: 110, attack: 1})
+unitsStore.addUnit({id: 'player1', line: 'front', width: 0.1, aspectRadio: 16/9, place: 1, team: 'player', hitPoints: 5, maxHitPoints: 5, speed: 100, attack: 1})
+unitsStore.addUnit({id: 'player2', line: 'front', width: 0.1, aspectRadio: 16/9, place: 2, team: 'player', hitPoints: 5, maxHitPoints: 5, speed: 100, attack: 1})
+unitsStore.addUnit({id: 'player3', line: 'back', width: 0.1, aspectRadio: 16/9, place: 1, team: 'player', hitPoints: 5, maxHitPoints: 5, speed: 100, attack: 1})
+unitsStore.addUnit({id: 'player4', line: 'back', width: 0.1, aspectRadio: 16/9, place: 2, team: 'player', hitPoints: 5, maxHitPoints: 5, speed: 100, attack: 1})
+unitsStore.addUnit({id: 'enemy1', line: 'back', width: 0.1, aspectRadio: 16/9, place: 1, team: 'enemy', hitPoints: 4, maxHitPoints: 4, speed: 90, attack: 1})
+unitsStore.addUnit({id: 'enemy2', line: 'back', width: 0.1, aspectRadio: 16/9, place: 2, team: 'enemy', hitPoints: 4, maxHitPoints: 4, speed: 110, attack: 1})
+unitsStore.addUnit({id: 'enemy3', line: 'front', width: 0.1, aspectRadio: 16/9, place: 1, team: 'enemy', hitPoints: 4, maxHitPoints: 4, speed: 110, attack: 1})
+unitsStore.addUnit({id: 'enemy4', line: 'front', width: 0.1, aspectRadio: 16/9, place: 2, team: 'enemy', hitPoints: 4, maxHitPoints: 4, speed: 110, attack: 1})
 
 let currentUnitObj = null;
 $: {
@@ -53,7 +57,23 @@ let currentMove = 0;
 $: {
     if (currentUnitObj && currentUnitObj.team === 'enemy') {
         helpers.delayCall(() => {
-            const lowestHpPlayerObj = $unitsStore.filter(obj => obj.team === 'player').sort((a, b) => a.hitPoints - b.hitPoints)
+            // pobieramy wszystkie postacie playera
+            let availableUnitsToAttack = $unitsStore.filter(obj => obj.team === 'player');
+            if (currentUnitObj.line === 'front') {
+                // jezeli jestesmy na froncie to tylko frontowe mozemy zaatakowac
+                const playersFront = availableUnitsToAttack.filter(obj => obj.line === 'front');
+
+                // chyba ze nie froncie nie ma juz nikogo to bierzemy back
+                if(playersFront.length === 0) {
+                    availableUnitsToAttack = availableUnitsToAttack.filter(obj => obj.line === 'back');
+                } else {
+                    availableUnitsToAttack = playersFront;
+                }
+            } else {// todo: dorobic prowokacje
+
+            }
+            const lowestHpPlayerObj = availableUnitsToAttack.sort((a, b) => a.hitPoints - b.hitPoints)
+
             unitsStore.attack(currentUnitObj.id, lowestHpPlayerObj[0].id);
             nextMove();
         }, 0.1)
@@ -67,31 +87,50 @@ function nextMove() {
     }
 }
 
+function checkIfCanPlayerAttackUnit(unit) {
+    if (currentUnitObj && currentUnitObj.team === 'player' && unit.team === 'enemy') {
+        if (currentUnitObj.line === 'front') {
+            const frontEnemies = $unitsStore.filter(obj => obj.line === 'front');
+            if (frontEnemies.length === 0) {
+                return true;
+            } else {
+                if (unit.line === 'front') {
+                    return true;
+                }
+            }
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 
 let spaceBetweenUnits = 0;
 let startingPointOfPlayer = 0;
 let startingPointOfEnemy = 0;
 $: {
     spaceBetweenUnits = contentWidth * 0.1;
-    startingPointOfPlayer = 0;
-    startingPointOfEnemy = contentWidth * 1;
+    startingPointOfPlayer = contentWidth * 0.2;
+    startingPointOfEnemy = contentWidth * 0.8;
 }
 
 onMount(() => {
-    console.log(contentWidth)
 })
+let hoveredUnitId = null;
 </script>
 
-<div style="
-      height: 100%;
-      aspect-ratio: 16/9;
-position: relative;
-margin: 0 auto;">
+<div class="test" style="
+        aspect-ratio: 16/9;
+        position: relative;
+        margin: 0 auto;">
     <div style="
-width: 100%;
-height: 100%;
+    border: 1px solid black;
+    width: 100%;
+    height: 100%;
       perspective: 100px;
-   transform-style: preserve-3d;
+    transform-style: preserve-3d;
       top: 0;
       left: 50%;
       transform: translateX(-50%);
@@ -102,15 +141,23 @@ height: 100%;
                 <div on:keydown={() => {}} class="{unit.id}"
                        style="position:absolute;
                        left:0; bottom:0;
+                       {hoveredUnitId === unit.id ? 'filter:drop-shadow(2px 4px 6px black);' : ''}
                        border:1px solid {currentUnitObj && currentUnitObj.id === unit.id ? 'red' : 'black'};
                        height: {unitWidth * 16/9}px;
                        width: {unitWidth}px;
-                       transform: translate3d({unit.team === 'player' ? (startingPointOfPlayer + (spaceBetweenUnits * unit.place) - (unitWidth * 0.5)) : (startingPointOfEnemy + (spaceBetweenUnits * unit.place) - (unitWidth * 0.5))}px, 0px, -{unit.place * 100}px)"
+                       transform: translate3d({unit.team === 'player' ? ((unit.line === 'front' ? startingPointOfPlayer : startingPointOfPlayer - spaceBetweenUnits * 1.5 ) + (spaceBetweenUnits * unit.place) - (unitWidth * 0.5)) : ((unit.line === 'front' ? startingPointOfEnemy : startingPointOfEnemy + spaceBetweenUnits * 1.5 )  + (spaceBetweenUnits * unit.place) - (unitWidth * 0.5))}px, 0px, -{unit.place * 100}px)"
                         on:click={() => {
                        if(currentUnitObj && currentUnitObj.team === 'player') {
                           unitsStore.attack(currentUnitObj.id, unit.id);
                           nextMove()
-                       }}}>
+                       }}}
+                     on:mouseover={() => {
+                         if (checkIfCanPlayerAttackUnit(unit)) {
+                             hoveredUnitId = unit.id;
+                         }
+                     }}
+                     on:mouseleave={() => { hoveredUnitId = null; }}
+                >
                     <img src="{images.unit}" style="width: 100%; height: auto;">
                     <div style="width: {(unit.hitPoints / unit.maxHitPoints * 100)}%; height: 25px; background: green; position: absolute; bottom: 0;">
                     </div>
@@ -120,3 +167,19 @@ height: 100%;
         {/if}
     </div>
 </div>
+
+<style lang="scss">
+  @media (min-aspect-ratio: 16/9) {
+    .test {
+      width: auto;
+      height: 100%;
+    }
+  }
+
+  @media (max-aspect-ratio: 16/9) {
+    .test {
+      width: 100%;
+      height: auto;
+    }
+  }
+</style>
