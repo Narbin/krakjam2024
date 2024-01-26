cb6eba8c2e995a8056b4dea061627681c203e2a8
Fusu <Arkadiusz.fus@codebind.pl>
2024-01-26 20:01:58 +0100

    Update core.ts

--- src/helpers/core.ts
+++ src/helpers/core.ts
@@ -1,11 +1,5 @@
 import {gsap} from "gsap";
 import * as helpers from "src/helpers/index";
-import timeStore from "src/stores/time";
-import {get} from "svelte/store";
-import {ExpoScaleEase} from "gsap/EasePack";
-import settingsStore from "src/stores/settings";
-import {v4 as UUIDv4} from "uuid";
-import adventuresEventsStore from "src/stores/adventuresEvents";
 
 export function initThirdParty() {
     gsap.config({
